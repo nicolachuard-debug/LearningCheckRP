4af1e4729848c4d558836e50b09e4e59b4a02b49
nicolachuard-debug <nicola.chuard@gmail.com>
2026-08-05 14:21:06 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
-FIREBASE_SERVICE_ACCOUNT_JSON = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON", "")
-PORT = int(os.getenv("PORT", "8000"))
